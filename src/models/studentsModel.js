ae0bdfc11ff7344f6f16c357f55ff19c6706b8a9
Munsur Rahman <munsurinfo@gmail.com>
2022-09-25 19:54:27 +0600

    Noted

--- src/models/studentsModel.js
+++ src/models/studentsModel.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose')
 
 const DataSchema = mongoose.Schema({
-    Name:String,
-    Roll:String,
-    Class:String,
-    Remarks:String
-})
+    Name:{type:String},
+    Roll: {
+        type:Number,
+        min:[6,'Min 6 & Max 12, but Supplied Value is={VALUE}'],
+        max:[12,'Min 6 & Max 12, but Supplied Value is={VALUE}'],
+        required:true,
+        unique:true,
+    },
+    Class: {type:String},
+    Remarks: {type:String,default:"No Remarks"}
+},{versionKey:false})
 
 const StudentsModel = mongoose.model('students',DataSchema);
 module.exports = StudentsModel;
